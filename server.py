27a69a7c719aae96768e7b89efc1ffd55cca701d
kchow3 <kchow3@ualberta.ca>
2016-02-24 12:36:56 -0700

    fixed index.html

--- server.py
+++ server.py
@@ -100,7 +100,7 @@ def get_entity(entity):
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
-    return make_response('YOU HAVE DESTROYED THE WORLD')
+    return make_response('YOU HAVE DESTROYED THE WORLD!', 200)
 
 if __name__ == "__main__":
     app.run()
